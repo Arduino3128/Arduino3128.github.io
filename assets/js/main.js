efe99d503eca9521b3146647e67d4095a20486d0
Kanad Nemade <kanadnemade88@gmail.com>
2021-05-17 16:53:28 +0530

    Added Preloader.

--- assets/js/main.js
+++ assets/js/main.js
@@ -158,21 +158,26 @@
     }
   });
 
+  window.addEventListener("load", function(){
+    const loader = document.querySelector(".loader");
+    loader.className+=" hidden";
+    const typed = select('.typed')
+    if (typed) {
+      let typed_strings = typed.getAttribute('data-typed-items')
+      typed_strings = typed_strings.split(',')
+      new Typed('.typed', {
+        strings: typed_strings,
+        loop: true,
+        typeSpeed: 100,
+        backSpeed: 50,
+        backDelay: 2000
+      });
+    }
+  });
   /**
    * Hero type effect
    */
-  const typed = select('.typed')
-  if (typed) {
-    let typed_strings = typed.getAttribute('data-typed-items')
-    typed_strings = typed_strings.split(',')
-    new Typed('.typed', {
-      strings: typed_strings,
-      loop: true,
-      typeSpeed: 100,
-      backSpeed: 50,
-      backDelay: 2000
-    });
-  }
+  
 
   /**
    * Testimonials slider
